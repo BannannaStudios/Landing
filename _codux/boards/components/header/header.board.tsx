74fa8ae34f05f8fc9a99e3fac24d5b9b78f517b7
Thoq <125930852+Thoq-jar@users.noreply.github.com>
2024-09-12 19:32:26 -0400

    header

--- _codux/boards/components/header/header.board.tsx
+++ _codux/boards/components/header/header.board.tsx
@@ -1,12 +1,13 @@
 import { createBoard } from '@wixc3/react-board';
 import ComponentWrapper from '_codux/board-wrappers/component-wrapper';
 import { Header } from '~/components/header/header';
+import styles from './header.board.module.scss';
 
 export default createBoard({
     name: 'Header',
     Board: () => (
         <ComponentWrapper>
-            <Header />
+            <Header className={styles.header} />
         </ComponentWrapper>
     ),
 });
